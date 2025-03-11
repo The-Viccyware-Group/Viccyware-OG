6648cf02fb5ae49b4909cf0f0d479f4556e3a22d
Raj-jyot Singh Deol <deolrajjyot@gmail.com>
2025-03-11 11:34:23 -0400

    Fix eyes on 1.0 units

--- cannedAnimLib/proceduralFace/proceduralFace.cpp
+++ cannedAnimLib/proceduralFace/proceduralFace.cpp
@@ -28,7 +28,7 @@ const int ProceduralFace::NominalEyeHeight = IsXray() ? 50 : 57;
 const int ProceduralFace::NominalEyeWidth = IsXray() ? 38 : 43;
 // const int ProceduralFace::NominalEyeHeight = 57;
 // const int ProceduralFace::NominalEyeWidth = 43;
-const float ProceduralFace::DefaultSizeXY= IsXray() ? 0.95f : 1.1f; //Default size of the eyes (applies both X and Y)
+const float ProceduralFace::DefaultSizeXY= IsXray() ? 0.90f : 1.0f; //Default size of the eyes (applies both X and Y)
 
 ProceduralFace* ProceduralFace::_resetData = nullptr;
 ProceduralFace* ProceduralFace::_blankFaceData = nullptr;
@@ -64,7 +64,7 @@ void ProceduralFace::SaturationConsoleFunction(ConsoleFunctionContextRef context
 namespace {
 # define CONSOLE_GROUP "Face.ParameterizedFace"
 
-  CONSOLE_VAR_RANGED(s32, kProcFace_NominalEyeSpacing, CONSOLE_GROUP, IsXray() ? 86 :83, -FACE_DISPLAY_WIDTH, FACE_DISPLAY_WIDTH);  // V1: 64;
+  CONSOLE_VAR_RANGED(s32, kProcFace_NominalEyeSpacing, CONSOLE_GROUP, IsXray() ? 86 :92, -FACE_DISPLAY_WIDTH, FACE_DISPLAY_WIDTH);  // V1: 64;
 
 # undef CONSOLE_GROUP
 
