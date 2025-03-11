8a09b0a668771a0a4c5048f16885c2593f22e67d
Yrekcaz <yrekcaz9@gmail.com>
2025-03-10 17:57:02 -0700

    update links to anki.bot

--- animProcess/src/cozmoAnim/connectionFlow.cpp
+++ animProcess/src/cozmoAnim/connectionFlow.cpp
@@ -44,7 +44,7 @@ namespace {
 u32 _pin = 123456;
 
 const f32 kRobotNameScale = 0.7f;
-const std::string kURL = "v.vicw.xyz";
+const std::string kURL = "anki.bot/v";
 const ColorRGBA   kColor(0.f, 0.804f, 1.f, 1.f);
 
 const char* kShowPinScreenSpriteName = "pairing_icon_key";
